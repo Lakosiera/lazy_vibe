60065e2e6eb9fe99d38ff7132148a1039fd26a58
Lakosiera <lakosiera@mail.ru>
2025-04-21 23:07:18 +0500

    инициация проектов

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-})
+    server: {
+      proxy: {
+        "/api": {
+          target: `http://${env.BACKEND_URL}`,
+          changeOrigin: true,
+          secure: false,
+          rewriteWsOrigin: true,
+          // rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
+    }
+  })
+}
