551e1ac1620b99aa71c46dc19adc2cf5094664be
Lakosiera <lakosiera@mail.ru>
2025-04-24 18:22:36 +0500

    vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,25 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import LogoIcon from './ui/icons/IconLogo.vue'
 </script>
 
 <template>
-    <img alt="Vue logo" class="logo" src="@/assets/lazyvibe.svg" width="256" height="256" />
-  <!-- <header>
+  <LogoIcon class="logo"/>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container-fluid py-3">
+    <!-- <div class="card _text-bg-light shadow-lg" style="width: 18rem;"> -->
+      <img alt="Vue logo" class="logo card-img-top" src="@/assets/lazyvibe.svg" width="256" height="256" />
+      <!-- <div class="card-body">
+      </div>
+    </div> -->
+  </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header> -->
+  <div class="neon"></div>
 
-  <RouterView />
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
