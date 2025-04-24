6be03a440eed35003d94a73ae3e260ff52350cdb
Lakosiera <lakosiera@mail.ru>
2025-04-24 21:01:19 +0500

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default ({ mode }: UserConfig) => {
-  const env = loadEnv(mode || , process.cwd(), '');
+  const env = loadEnv(mode || '', process.cwd(), '');
 
   return defineConfig({
     base: `${env.BASE}`,
