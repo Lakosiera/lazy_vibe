cebcdd84a333dd1f93c1895c5ddfc6233b4f78a5
Lakosiera <lakosiera@mail.ru>
2025-04-25 20:47:14 +0500

    оформление

--- frontend/src/ui/views/IdeaCreateView.vue
+++ frontend/src/ui/views/IdeaCreateView.vue
@@ -5,7 +5,34 @@
 <template>
     <div class="container-fluid py-3">
         <div class="container">
-            Есть идея!
+            <div class="row justify-content-md-center">
+                <div class="col-lg-8">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Есть идея!</h5>
+
+<form>
+    <div class="mb-3">
+        <label for="input1" class="form-label">Навание</label>
+        <input type="text" class="form-control" id="input1" aria-describedby="help1">
+        <div id="help1" class="form-text">Короткое название для отобажения</div>
+    </div>
+    <div class="mb-3">
+        <label for="input2" class="form-label">Идея</label>
+        <textarea class="form-control" id="input2" rows="8"  aria-describedby="help2"></textarea>
+        <div id="help2" class="form-text">Описть идею своими слолвами</div>
+    </div>
+
+    <div class="d-grid gap-2">
+        <button type="submit" class="btn btn-primary">Подумай за меня!</button>
+        <button type="submit" class="btn btn-success">Поделиться идеей</button>
+    </div>
+</form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
--- frontend/src/ui/views/IdeaSearchView.vue
+++ frontend/src/ui/views/IdeaSearchView.vue
@@ -4,7 +4,55 @@
 <template>
     <div class="container-fluid py-3">
         <div class="container">
-            Подайте мне идею!
+            <div class="card shadow">
+
+                <div class="card-header p-2">
+                    <div class="input-group">
+                        <span class="input-group-text" id="basic-addon1">@</span>
+                        <input type="text" class="form-control" placeholder="Подайте мне идею!" aria-label="Username"
+                            aria-describedby="basic-addon1">
+                    </div>
+                </div>
+                <div class="card-body p-0 row">
+                    <div class="col _table-responsive">
+
+                    <table class="table _table-sm _table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Идея</th>
+                                <th scope="col">Описание</th>
+                                <th scope="col">Сроки</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-group-divider _text-nowrap _text-truncate">
+                            <tr>
+                                <th scope="row">1</th>
+                                <td class="">Кормушка для кота</td>
+                                <td class="">
+                                    Хочу сделать умную кормушку для кота, которая будет давать еду по расписанию и отправлять мне фото в Telegram.
+                                </td>
+                                <td class="">1-2 месяца</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td class="">Программировать</td>
+                                <td class="">Хочу найчиться программировать</td>
+                                <td class="">1 год</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td class="">Владыка мира</td>
+                                <td class="">Хочу стать повелителем мира!</td>
+                                <td class="">1000 лет</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
